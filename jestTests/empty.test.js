18cd597a6c4eb722f7abe0fb1e990e461037001b
Amal Dusalin <adusalin@fil-it.ru>
2021-10-31 18:32:16 +0300

    Добавил тесты

---
+++ jestTests/empty.test.js
@@ -0,0 +1,7 @@
+test('Should be passed', () => {
+    expect(1 + 2).toBe(3);
+});
+
+test('Should be passed 2', () => {
+  expect(1 + 2).toBe(3);
+});
---
+++ tests.js
@@ -0,0 +1,44 @@
+import util from 'util'
+import { exec as syncExec } from 'child_process';
+const exec = util.promisify(syncExec);
+import fetch from 'node-fetch'
+
+async function docker() {
+    const { stdout: newReleaseTagWithN } = await exec('git describe --tags')
+    const newReleaseTag = newReleaseTagWithN.replace('\n', '')
+    const findJson = {
+        filter: {
+            queue: "TMP",
+            summary: `Dekart hw8 ${newReleaseTag}`,
+        }
+    }
+    const find = await fetch('https://api.tracker.yandex.net/v2/issues/_search', {
+        method: 'POST',
+        body: JSON.stringify(findJson),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `OAuth ${process.env.API_TOKEN}`,
+            'X-Org-ID': process.env.API_ID
+        }
+    })
+    const response = await find.json()
+    const ticketId = response.at(0).id
+
+    const { stderr } = await exec('npm run test')
+
+    const commentJson = {
+        "text": stderr
+    }
+
+    await fetch(`https://api.tracker.yandex.net/v2/issues/${ticketId}/comments`, {
+        method: 'POST',
+        body: JSON.stringify(commentJson),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `OAuth ${process.env.API_TOKEN}`,
+            'X-Org-ID': process.env.API_ID
+        }
+    })
+}
+
+docker()
