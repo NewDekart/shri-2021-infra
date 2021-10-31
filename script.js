02ee6ce17ca1123f61bbb7b866d9a69aff9bf2dc
Amal Dusalin <adusalin@fil-it.ru>
2021-10-31 15:46:03 +0300

    перевел скрипт на js

---
+++ script.js
@@ -0,0 +1,41 @@
+import util from 'util'
+import { exec as syncExec } from 'child_process';
+const exec = util.promisify(syncExec);
+import fetch from 'node-fetch'
+
+async function test() {
+    const { stdout: newReleaseTags } = await exec('git describe HEAD --tags')
+    const newReleaseTag =  newReleaseTags.replace('\n', '')
+    const { stdout: otherReleasesTags } = await exec(`git tag | grep -B1 ${newReleaseTag}`)
+    const oldReleaseTag = otherReleasesTags.match(/.+/).at(0)
+    const { stdout: changeLog } = await exec(`git log ${newReleaseTag}...${oldReleaseTag} --oneline`)
+    const actualChangeLogs = changeLog.replace('\n', '<br>')
+    const { stdout: lastTagInfo } = await exec(`git show ${newReleaseTag}`)
+    const author = lastTagInfo.match(/Author:(.+)\n/).at(1).trim()
+    const date = lastTagInfo.match(/Date:(.+)\n/).at(1).trim()
+    const createJson = {
+        queue: "TMP",
+        summary: `Dekart hw8 ${newReleaseTag}`,
+        type: "task",
+        description: `<#<html>
+            <head></head>
+            <body>
+                Release:&nbsp;${newReleaseTag}<br>
+                Author:&nbsp;${author}<br>
+                Date:&nbsp;${date}<br><br>
+                Changelog:<br><br>${actualChangeLogs}
+            </body>
+        </html>#>`
+    }
+    await fetch('https://api.tracker.yandex.net/v2/issues/', {
+        method: 'POST',
+        body: JSON.stringify(createJson),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth AQAAAAA74JZXAAd4ycKPSFr9UET4qtCw3Dhubj8',
+            'X-Org-ID': '6461097'
+        }
+    })
+}
+
+test()
